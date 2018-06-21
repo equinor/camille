078b741310117b31a0e76dec9875996e9fed1ba0
Jens Gåsemyr Magnus <jegm@statoil.com>
2018-06-21 17:01:59 +0200

    If options are missing raise ValueError over ArgumentError
    There is no built-in exception named ArgumentError in Python

--- camille/processors/rolling.py
+++ camille/processors/rolling.py
@@ -3,7 +3,7 @@ from pandas import DataFrame
 def process(df, **kwargs):
     options = ('window', 'interpolation')
     if not all([key in options for key in kwargs.keys()]):
-        raise ArgumentError('Unknown argument(s) {}'.format(kwargs.keys() - options))
+        raise ValueError('Unknown argument(s) {}'.format(kwargs.keys() - options))
     window = kwargs.get('window', 10)
     interpolation = kwargs.get('interpolation', 'triang')
     return DataFrame(df.rolling(window, win_type=interpolation).mean())
