7323721f45170c78aa6823fe8108ef2806862784
Jens Gåsemyr Magnus <jegm@statoil.com>
2018-10-02 15:32:36 +0200

    Rename modules
    * Rename camille.processors -> camille.process
             camille.sources -> camille.source
             camille.utils -> camille util
    
    Move processor tests to their own directory
    
    * We will add source and util sub-packages that will have their own tests.

--- camille/__init__.py
+++ camille/__init__.py
@@ -2,10 +2,4 @@
 
 """
 
-from __future__ import absolute_import
-
-from .processors import fft
-from .processors import mooring_fatigue
-from .processors import rolling
-
 __version__ = '0.0.1'
---
+++ camille/util/__init__.py
@@ -0,0 +1,1 @@
+from .sncurves import sn_curve
