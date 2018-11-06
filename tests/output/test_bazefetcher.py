ba0fd30d52eda8b9dd6fde0c8c2f758b5b810090
Jens Gåsemyr Magnus <jegm@statoil.com>
2018-11-06 20:41:22 +0100

    Bazefetcher output fixes
    The changes to `camille.source.bazefetcher` revealed some issues in the
    testing of `camille.output.bazefetcher`.

--- tests/output/test_bazefetcher.py
+++ tests/output/test_bazefetcher.py
@@ -1,67 +1,69 @@
+from datetime import datetime
+from pytz import utc
+import camille.output
+import camille.source
+import numpy as np
 import os
 import pandas as pd
-import numpy as np
-import datetime
-import pytz
-import camille.source
-import camille.output
 
 
-def is_correctly_loaded_from_basefetcher( expected, tag, basedir ):
-    bazein = camille.source.bazefetcher( basedir )
-    result = bazein( tag, expected.index[0], expected.index[-1] )
-
-    return (expected.index == result.index).all() \
-       and np.allclose( expected.values, result.values )
+def assert_correctly_loaded(expected, tag, basedir, t0, t1):
+    bazein = camille.source.bazefetcher(basedir)
+    result = bazein(tag, t0, t1)
+    pd.testing.assert_series_equal(expected, result)
 
 
 def test_one_day_one_file(tmpdir):
-    rng = pd.date_range( start='1/1/2018T10:00',
-                         end='1/1/2018T12:00',
-                         freq='H', tz='utc' )
-    ts = pd.Series(np.random.randn(len(rng)), index=rng)
+    t0 = datetime(2018, 1, 1, 10, tzinfo=utc)
+    t1 = datetime(2018, 1, 1, 13, tzinfo=utc)
+    rng = pd.date_range(t0, t1, freq='H', name='time', closed='left')
+    ts = pd.Series(np.random.randn(len(rng)), name='value', index=rng)
 
-    bazeout = camille.output.bazefetcher( str(tmpdir) )
-    bazeout( ts, 'test', ts.index[0], ts.index[-1] )
+    bazeout = camille.output.bazefetcher(str(tmpdir))
+    bazeout(ts, 'test', t0, t1)
 
-    assert os.listdir( os.path.join( str(tmpdir), 'test' ) ) \
-        == ['test_2018-01-01T00.00.00+00.00_2018-01-02T00.00.00+00.00.json.gz']
-    assert is_correctly_loaded_from_basefetcher( ts, 'test', str(tmpdir) )
+    assert (os.listdir(os.path.join( str(tmpdir), 'test' ))
+        == ['test_2018-01-01T00.00.00+00.00_2018-01-02T00.00.00+00.00.json.gz'])
+    assert_correctly_loaded(ts, 'test', str(tmpdir), t0, t1)
 
 
 def test_two_days_two_files(tmpdir):
-    rng = pd.date_range( start='1/1/2018T23:00',
-                         end='1/2/2018T02:00',
-                         freq='H', tz='utc' )
-    ts = pd.Series(np.array([1,2,3,4]), index=rng)
+    t0 = datetime(2018, 1, 1, 23, tzinfo=utc)
+    t1 = datetime(2018, 1, 2, 3, tzinfo=utc)
+    rng = pd.date_range(t0, t1, freq='H', name='time', closed='left')
+    ts = pd.Series(np.array([1,2,3,4]), name='value',index=rng)
 
-    bazeout = camille.output.bazefetcher( str(tmpdir) )
-    bazeout( ts, 'test', ts.index[0], ts.index[-1] )
+    bazeout = camille.output.bazefetcher(str(tmpdir))
+    bazeout(ts, 'test', t0, t1)
 
-    assert os.listdir( os.path.join( str(tmpdir), 'test' ) ) \
-        == ['test_2018-01-01T00.00.00+00.00_2018-01-02T00.00.00+00.00.json.gz',
-            'test_2018-01-02T00.00.00+00.00_2018-01-03T00.00.00+00.00.json.gz']
+    flist = os.listdir(os.path.join(str(tmpdir), 'test'))
+    flist.sort()
+    assert len(flist) == 2
+    assert (flist[0]
+        == 'test_2018-01-01T00.00.00+00.00_2018-01-02T00.00.00+00.00.json.gz')
+    assert (flist[1]
+        == 'test_2018-01-02T00.00.00+00.00_2018-01-03T00.00.00+00.00.json.gz')
 
-    assert is_correctly_loaded_from_basefetcher( ts, 'test', str(tmpdir) )
+    assert_correctly_loaded(ts, 'test', str(tmpdir), t0, t1)
 
 
 def test_output_interval(tmpdir):
-    rng = pd.date_range( start='1/1/2018T12:00',
-                         end='1/1/2018T16:00',
-                         freq='H', tz='utc' )
+    t00 = datetime(2018, 1, 1, 12, tzinfo=utc)
+    t10 = datetime(2018, 1, 2, 4, tzinfo=utc)
+    rng = pd.date_range(t00, t10, freq='H', name='time', closed='left')
     ts = pd.Series(np.random.randn(len(rng)), index=rng)
 
-    start = datetime.datetime( 2018, 1, 1, 13, 30, tzinfo=pytz.utc )
-    end = datetime.datetime( 2018, 1, 1, 15, 30, tzinfo=pytz.utc )
+    t01 = datetime(2018, 1, 1, 13, 30, tzinfo=utc)
+    t11 = datetime(2018, 1, 1, 15, 30, tzinfo=utc)
 
-    bazeout = camille.output.bazefetcher( str(tmpdir) )
-    bazeout( ts, 'test', start, end )
+    bazeout = camille.output.bazefetcher(str(tmpdir))
+    bazeout(ts, 'test', t01, t11)
 
-    bazein = camille.source.bazefetcher( str(tmpdir) )
-    result = bazein( 'test', ts.index[0], ts.index[-1] )
+    bazein = camille.source.bazefetcher(str(tmpdir))
+    result = bazein('test', t00, t10)
 
     assert result.size == 2
 
-    expected_times = [ datetime.datetime( 2018, 1, 1, 14, tzinfo=pytz.utc),
-                       datetime.datetime( 2018, 1, 1, 15, tzinfo=pytz.utc) ]
+    expected_times = [datetime(2018, 1, 1, 14, tzinfo=utc),
+                      datetime(2018, 1, 1, 15, tzinfo=utc)]
     assert (result.index == expected_times).all()
