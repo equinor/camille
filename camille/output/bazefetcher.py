ffcfbc4963d1f3f461dbe47001f8de43df855095
Alena Chaikouskaya (IT SI SIB) <alec@equinor.com>
2018-11-16 13:23:32 +0100

    Bazefetcher output sometimes writes one file to many

--- camille/output/bazefetcher.py
+++ camille/output/bazefetcher.py
@@ -4,40 +4,49 @@ import pytz
 import pandas as pd
 
 def _to_midnight_utc(timestamp):
-    """ Copied from bazefetcher
+    """ Copied from bazefetcher, logic modified
 
-    Converts a timestamp or date to an UTC timestamp. Dates are converted to
-    midnight the given date (00:00:00).
-    All timestamps are converted to UTC if they have timezone information,
-    and assumed to already be UTC if they have no timezone information.
+    Converts a timestamp to an UTC timestamp, to midnight of the
+    given date (00:00:00). All timestamps are converted to UTC if they
+    have timezone information, and assumed to already be UTC if they
+    have no timezone information.
     """
-    if (isinstance(timestamp, datetime.date)
-        and not isinstance(timestamp, datetime.datetime)):
-        timestamp = datetime.datetime(
-            timestamp.year,
-            timestamp.month,
-            timestamp.day)
     try:
         timestamp = pytz.utc.localize(timestamp)
     except ValueError:
         timestamp = timestamp.astimezone(pytz.utc)
+    timestamp = timestamp.replace(hour=0, minute=0, second=0)
     return timestamp
 
 
 def _daterange(start_date, end_date):
-    start_date = _to_midnight_utc( start_date.date() )
-    end_date = _to_midnight_utc( end_date.date() )
-
-    for d in range(int((end_date - start_date).days)+1):
-        start = start_date + datetime.timedelta(d)
-        end = start + datetime.timedelta(1)
-        yield (start,end)
+    """
+    Generates dates in range between start_date and end_date with one
+    day difference starting from start_date.
+    First calls _to_midnight_utc on both.
+    :param start_date: Start date timestamp, inclusive.
+    :param end_date: End date timestamp, exclusive. Note, that if end
+    time date is later than midnight of that day, the midnight date
+    will be generated. If the date is exactly at midninght,
+    it's not generated
+    :return:
+    """
+    if start_date != end_date:
+        is_end_on_midnight = _to_midnight_utc(end_date) == end_date
+        start_date = _to_midnight_utc(start_date)
+        end_date = _to_midnight_utc(end_date)
+        if is_end_on_midnight:
+            end_date -= datetime.timedelta(days=1)
+        while start_date <= end_date:
+            next_day = start_date + datetime.timedelta(days=1)
+            yield (start_date, next_day)
+            start_date = next_day
 
 
 def _generate_tag_location(
     root, tag_name, start_date, end_date, full_path=True, suffix=".json"
     ):
-    """ Copied from bazefetcher
+    """ Copied from bazefetcher, logic modified
 
     Generates and returns the path for storing a tag given a start and end
     date. With full_path=False it will return the relative path to the
@@ -45,8 +54,8 @@ def _generate_tag_location(
      """
     filename = "{}_{}_{}{}".format(
         tag_name,
-        _to_midnight_utc(start_date).isoformat().replace(":", "."),
-        _to_midnight_utc(end_date).isoformat().replace(":", "."),
+        start_date.isoformat().replace(":", "."),
+        end_date.isoformat().replace(":", "."),
         suffix)
     directory_name = tag_name
     path = os.path.join(directory_name, filename)
@@ -61,6 +70,21 @@ def bazefetcher(root):
 
     def bazefetcher_internal(
             series, tag=None, start=None, end=None):
+        """
+        Stores the provided series of data under root folder specified
+        during call of outer method.
+        :param series: Indexed series of datetime information.
+        Intended object: pandas.Series
+        :param tag: Tag of the document, Must be specified
+        :param start: Start datetime of the stored period (inclusive).
+        If param is not provided, start element of series parameter is
+        taken. Date should be timezone aware
+        :param end: End datetime of the stored period (exclusive).
+        If param is not provided, end element of series parameter is
+        taken. Date should be timezone aware. Can't be earlier than
+        start date param.
+        :return: None
+        """
         if tag is None:
             raise ValueError('tag must be specified')
 
@@ -78,7 +102,6 @@ def bazefetcher(root):
         eps = datetime.timedelta(microseconds=1)
 
         for s, e in _daterange( start, end ):
-
             tag_path = _generate_tag_location( root,
                                               tag,
                                               s,
