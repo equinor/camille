b4465f23551da2ce082aa6e0247cb3b8ccd19d8e
Jens Gåsemyr Magnus <jegm@equinor.com>
2018-12-03 17:16:30 +0100

    Use versioning compatible with sphinx

--- camille/__init__.py
+++ camille/__init__.py
@@ -1,4 +1,18 @@
 """Camille is a timeseries processing toolbox.
 
 """
+
+try:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    pass
+
 from . import process, util, output, source
+
+__all__ = [
+    'process',
+    'util',
+    'output',
+    'source',
+]
