9bc740b157eea36672b7ee5f50c165b1f24aa4dc
Alena Chaikouskaya <alec@equinor.com>
2018-12-04 15:14:29 +0100

    Add delta_temp docstring
    Added docs for delta_temp process

--- camille/process/delta_temp.py
+++ camille/process/delta_temp.py
@@ -1,4 +1,18 @@
 def process(amb, sea):
+    """Process temperatures delta
+
+    Parameters
+    ----------
+    amb : pandas.Series
+        Ambient temperature
+    sea : pandas.Series
+        Sea temperature
+
+    Returns
+    -------
+    pandas.Series
+        Difference between provided series resampled per 20 minutes
+    """
     delta_temp  = sea.resample('20T').mean() - amb.resample('20T').mean()
     delta_temp.rename('delta_temp', inplace=True)
     return delta_temp
