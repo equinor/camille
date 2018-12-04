4592fafe692070cc29d9e3dc607ca814f559d5a1
Alena Chaikouskaya <alec@equinor.com>
2018-12-04 15:38:34 +0100

    Add atm_stb docstring
    Added docs for atm_stb process

--- camille/process/atm_stb.py
+++ camille/process/atm_stb.py
@@ -3,12 +3,38 @@ import math
 
 
 def process(deltatemp):
+    """Process atmospheric stability
+
+    Classify atmospheric stability based on `deltatemp`
+
+    Parameters
+    ----------
+    deltatemp : pandas.Series
+        Temperature difference
+
+    Returns
+    -------
+    pandas.Series
+        Atmospheric stability series
+    """
     atm_stb = deltatemp.apply(_atm_stb)
     atm_stb.rename('atm_stb', inplace=True)
     return atm_stb
 
 
 def _atm_stb(delta_temp):
+    """Atmospheric stability
+
+    Parameters
+    ----------
+    delta_temp : number
+        Difference between ambient and sea temperatures
+
+    Returns
+    -------
+    str
+        Atmospheric stability classification
+    """
     if not delta_temp or math.isnan(delta_temp): return 'Missing Data'
     if 5.0 <= delta_temp         : return 'Very Unstable'
     if 2.5 <= delta_temp <   5.0 : return 'Unstable'
