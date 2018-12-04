b830fe36658614c5708641962899226627c48274
Andreas Jacobsen <anjac@statoil.com>
2018-12-04 15:10:09 +0100

    Trying out generating and uploading mutmut report to S3

---
+++ buildtools/mutmut_report.py
@@ -0,0 +1,38 @@
+from subprocess import check_output
+from json import dumps
+import os
+
+
+def create_report():
+    branch = "master"
+
+    mutmut_result = check_output(["mutmut", "results"])
+    survival_rate = extract_survival_rate(mutmut_result)
+    json_report = create_json_report(branch, survival_rate)
+    write_report(json_report)
+
+
+def extract_survival_rate(mutmut_result):
+    lines = mutmut_result.decode().split("\n")
+    before_survivers = [i for i, s in enumerate(lines) if "Survived" in s]
+    del lines[0:before_survivers[0]+1]
+
+    return len(lines)
+
+
+def create_json_report(branch, survival_rate):
+    dict_report = {"branches": {"branch": branch, "survival_rate": survival_rate}}
+
+    return dumps(dict_report)
+
+
+def write_report(json_report):
+    filename = 'mutmut/mutmut_report.json'
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, "w") as f:
+        f.write(json_report)
+
+
+if __name__ == '__main__':
+    create_report()
+
