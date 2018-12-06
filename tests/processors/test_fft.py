a8a853d7249833ad367e7bc751f6ef9f73de30ed
Jens Gåsemyr Magnus <jegm@equinor.com>
2018-12-06 17:13:07 +0100

    Add docstring to fft processor

--- tests/processors/test_fft.py
+++ tests/processors/test_fft.py
@@ -6,19 +6,15 @@ from camille import process
 
 def test_fft():
     df = pd.DataFrame(np.random.normal(size=(100)))
-    config = {'inverse': False}
-    inv_config = {'inverse': True}
-    spectrum = process.fft(df, **config)
-    signal = process.fft(spectrum, **inv_config).apply(np.real)
+    spectrum = process.fft(df, inverse=False)
+    signal = process.fft(spectrum, inverse=True).apply(np.real)
 
     assert np.allclose(df, signal)
 
 
 def test_fft2D():
     df = pd.DataFrame(np.random.normal(size=(100,10)))
-    config = {'inverse': False}
-    inv_config = {'inverse': True}
-    spectrum = process.fft(df, **config)
-    signal = process.fft(spectrum, **inv_config).apply(np.real)
+    spectrum = process.fft(df, inverse=False)
+    signal = process.fft(spectrum, inverse=True).apply(np.real)
 
     assert np.allclose(df, signal)
