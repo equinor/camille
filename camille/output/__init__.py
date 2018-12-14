42a5c23ce0999d4f0112a401bdef9e85fad99182
Alena Chaikouskaya <alec@equinor.com>
2018-12-14 12:01:45 +0100

    Add docs for output module

--- camille/output/__init__.py
+++ camille/output/__init__.py
@@ -1,4 +1,16 @@
-"""Outputs
+"""\
+Outputs
+==========
+This module is a collection of functions producing outputs.
+
+Notes
+-----
+Used data formats might be too specific, so currently module is mainly
+intended for internal use
+
+Available functions
+--------------------
+* :func:`~camille.output.bazefetcher`
 """
 
 from .bazefetcher import bazefetcher
