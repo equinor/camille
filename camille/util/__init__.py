248c1033719de7b4ff70a5e35b618d39d0d5dae4
Alena Chaikouskaya <alec@equinor.com>
2018-12-14 12:07:21 +0100

    Add docs for util module

--- camille/util/__init__.py
+++ camille/util/__init__.py
@@ -1,4 +1,13 @@
-"""Util
+"""\
+Utils
+==========
+This module is a collection of utility functions.
+
+Available functions
+-------------------
+* :func:`~camille.util.baze_iterator`
+* :func:`~camille.util.resample`
+* :func:`~camille.util.sn_curve`
 """
 
 from .baze_iterator import baze_iterator
